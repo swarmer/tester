57202ebd34609f89c98775665e32938807a3ab0a
Anton Barkovsky <anton@swarmer.me>
2015-06-14 00:19:28 +0300

    Setup the main app

--- src/tester/urls.py
+++ src/tester/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from core import views
+
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
 ]
