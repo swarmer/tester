eed18fa95032f01ca62f8526a2498b9be80ab0e4
Anton Barkovsky <anton@swarmer.me>
2015-06-14 20:00:48 +0300

    Add templates and core url map

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+from . import views
+
+
+urlpatterns = patterns('core.views',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
+)
--- tester/urls.py
+++ tester/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from core import views
+import core.urls
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index),
+    url(r'^', include(core.urls)),
 ]
