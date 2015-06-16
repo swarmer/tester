21acb054773e0553c98cd3a4f7dd3c3e06b8f63e
Anton Barkovsky <anton@swarmer.me>
2015-06-16 16:46:25 +0300

    Add login page

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 
 urlpatterns = patterns('core.views',
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^logout', auth_views.logout, {'next_page': '/'}),
+    url(r'^login', auth_views.login),
 
     url(r'^$', views.index),
     url(r'^explore/$', views.explore),
--- tester/settings.py
+++ tester/settings.py
@@ -101,3 +101,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Authentication settings
+
+LOGIN_REDIRECT_URL = '/'
