4b825bd00b712e0649d7877ac9884b3588113977
Anton Barkovsky <anton@swarmer.me>
2015-06-19 03:14:51 +0300

    Add question ranges

--- core/static/core/js/test.js
+++ core/static/core/js/test.js
@@ -1,19 +1,66 @@
 "use strict";
 
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function randint(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function selectRandom() {
-    var questionElements = document.getElementById("questions").children;
+function getQuestions() {
     var questions = [];
-    for (var i = 0; i < questionElements.length; ++i)
-        questions.push(questionElements[i].textContent);
+    $("#questions").children().each(function () {
+        questions.push($(this).text());
+    });
 
-    var randomIndex = getRandomInt(0, questions.length - 1);
+    return questions;
+}
+
+function getIndices() {
+    var indicesString = $("#question-range").val();
+    if (indicesString === "")
+        return null;
+
+    var groups = indicesString.match(/^(\d+)-(\d+)$/);
+    if (groups === null) {
+        throw {
+            name: "InvalidRange",
+            message: "Invalid range string!"
+        };
+    }
+
+    var from = parseInt(groups[1]);
+    var to = parseInt(groups[2]);
+
+    return [from, to];
+}
+
+$("#next-button").click(function () {
+    var questions = getQuestions();
+    var from, to;
+    try {
+        var range = getIndices();
+        if (range === null) {
+            from = 1;
+            to = questions.length;
+        } else {
+            from = range[0];
+            to = range[1];
+        }
+
+        if (from < 1 || from > questions.length || to < from || to > questions.length) {
+            throw {
+                name: "InvalidRange",
+                message: "Range out of bounds!"
+            }
+        }
+    } catch (ex) {
+        $("#range-error-text").text(ex.message);
+        $("#range-error-box").show(100);
+        return;
+    }
+    $("#range-error-box").hide(100);
+
+    var randomIndex = randint(from - 1, to);
     var question = (randomIndex + 1) + ". " + questions[randomIndex];
 
-    var questionTextNode = document.getElementById("questionText");
-    questionTextNode.textContent = question;
-}
+    $("#question-text").text(question);
+});
