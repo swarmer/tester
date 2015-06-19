2c62fe615f9a8b0ac8720e6f19eb9fd6faa452df
Anton Barkovsky <anton@swarmer.me>
2015-06-19 15:29:39 +0300

    Add the list of user's tests

--- core/views.py
+++ core/views.py
@@ -4,8 +4,18 @@ from django.contrib.auth.models import User
 from .models import Test
 
 
+def index_list(request):
+    tests = Test.objects.filter(owner=request.user)
+    return render(request, 'index_list.html', {'tests': tests})
+
+def index_intro(request):
+    return render(request, 'index_intro.html')
+
 def index(request):
-    return render(request, 'index.html')
+    if request.user.is_authenticated():
+        return index_list(request)
+    else:
+        return index_intro(request)
 
 def explore(request):
     public_tests = Test.objects.filter(is_listed=True)
