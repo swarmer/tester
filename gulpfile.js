e8caf8bf461a7e7dd56259e23e8c177727d17251
Anton Barkovsky <anton@swarmer.me>
2015-08-18 15:29:04 +0300

    Use uglify

--- gulpfile.js
+++ gulpfile.js
@@ -10,23 +10,38 @@ var cssnano = require('cssnano');
 var postcss = require('gulp-postcss');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var merge = require('merge-stream');
 
 
 var staticFiles = ['core/static/**'];
 var buildDest = 'build_static/';
 
 function copyBowerPackage(name, files) {
-    gulp.src(path.join('bower_components/', name, files))
+    return gulp.src(path.join('bower_components/', name, files))
         .pipe(gulp.dest(path.join(buildDest, path.join('dist/', name))));
 }
 
-gulp.task('clean', function () {
-    del.sync(path.join(buildDest, '*'));
+gulp.task('clean', function (done) {
+    del(path.join(buildDest, '*'), done);
 });
 
 gulp.task('copyStatic', ['clean'], function () {
     return gulp.src(staticFiles)
         .pipe(gulp.dest(buildDest));
+});
+
+gulp.task('bower-install', ['clean'], function () {
+    return bower();
+});
+
+gulp.task('bower', ['bower-install'], function () {
+    return merge(
+        copyBowerPackage('bootstrap', 'dist/**'),
+        copyBowerPackage('jquery', 'dist/**'),
+        copyBowerPackage('js-cookie', 'src/**'),
+        copyBowerPackage('underscore', '*')
+    );
 });
 
 gulp.task('css', ['copyStatic'], function () {
@@ -42,23 +57,27 @@ gulp.task('css', ['copyStatic'], function () {
             zindex: false
         })
     ];
-    return gulp.src('build_static/*/*.css')
+    return gulp.src(path.join(buildDest, '*/*.css'))
         .pipe(sourcemaps.init({loadMaps: true}))
             .pipe(postcss(processors))
             .pipe(rename({extname: '.min.css'}))
         .pipe(sourcemaps.write('.', {sourceRoot: '/static/'}))
-        .pipe(gulp.dest('build_static/'));
+        .pipe(gulp.dest(buildDest));
 });
 
-gulp.task('bower', ['clean'], function () {
-    bower();
-    copyBowerPackage('bootstrap', 'dist/**');
-    copyBowerPackage('jquery', 'dist/**');
-    copyBowerPackage('js-cookie', 'src/**');
-    copyBowerPackage('underscore', '*');
+gulp.task('uglify', ['copyStatic', 'bower'], function() {
+  return gulp.src([
+        path.join(buildDest, '*/*.js'),
+        path.join(buildDest, '*/js-cookie/*.js')
+    ])
+    .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .pipe(rename({extname: '.min.js'}))
+    .pipe(sourcemaps.write('.', {sourceRoot: '/static/'}))
+    .pipe(gulp.dest(buildDest));
 });
 
-gulp.task('build', ['copyStatic', 'css', 'bower']);
+gulp.task('build', ['copyStatic', 'bower', 'css', 'uglify']);
 
 gulp.task('watch', function () {
     gulp.watch([staticFiles, 'bower.json'], ['build']);
