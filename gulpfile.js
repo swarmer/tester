605d9380528fc7998679992ee76b8ac8655fe4ab
Anton Barkovsky <anton@swarmer.me>
2015-08-18 04:57:51 +0300

    Add postcss

--- gulpfile.js
+++ gulpfile.js
@@ -5,44 +5,63 @@ var gutil = require('gulp-util');
 var del = require('del');
 var path = require('path');
 var bower = require('gulp-bower');
+var autoprefixer = require('autoprefixer-core');
+var cssnano = require('cssnano');
+var postcss = require('gulp-postcss');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 
 var staticFiles = ['core/static/**'];
 var buildDest = 'build_static/';
-
-
-function clean() {
-    del.sync(path.join(buildDest, '*'));
-}
-
-gulp.task('clean', clean);
-
 
 function copyBowerPackage(name, files) {
     gulp.src(path.join('bower_components/', name, files))
         .pipe(gulp.dest(path.join(buildDest, path.join('dist/', name))));
 }
 
-function build() {
-    gulp.src(staticFiles)
+gulp.task('clean', function () {
+    del.sync(path.join(buildDest, '*'));
+});
+
+gulp.task('copyStatic', ['clean'], function () {
+    return gulp.src(staticFiles)
         .pipe(gulp.dest(buildDest));
+});
 
+gulp.task('css', ['copyStatic'], function () {
+    var processors = [
+        autoprefixer({
+            browsers: 'last 2 versions'
+        }),
+        cssnano({
+            autoprefixer: false,
+            merge: false,
+            idents: false,
+            unused: false,
+            zindex: false
+        })
+    ];
+    return gulp.src('build_static/*/*.css')
+        .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(postcss(processors))
+            .pipe(rename({extname: '.min.css'}))
+        .pipe(sourcemaps.write('.', {sourceRoot: '/static/'}))
+        .pipe(gulp.dest('build_static/'));
+});
+
+gulp.task('bower', ['clean'], function () {
     bower();
-
     copyBowerPackage('bootstrap', 'dist/**');
     copyBowerPackage('jquery', 'dist/**');
     copyBowerPackage('js-cookie', 'src/**');
     copyBowerPackage('underscore', '*');
-}
+});
 
-gulp.task('build', ['clean'], build);
+gulp.task('build', ['copyStatic', 'css', 'bower']);
 
-
-function watch() {
-    gulp.watch(staticFiles, ['build']);
-}
-
-gulp.task('watch', watch);
-
+gulp.task('watch', function () {
+    gulp.watch([staticFiles, 'bower.json'], ['build']);
+});
 
 gulp.task('default', ['build']);
